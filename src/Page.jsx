65a18070d4d49390029d406ebc33604c8651bc43
yunusbalikci <balikci.yunus06@gmail.com>
2024-03-27 21:12:04 +0300

    first commit

---
+++ src/Page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Page() {
+  return (
+    <div className='md:flex flex-wrap'>
+        <img src="src/assets/menu_page-0001.jpg" className='w-full h-full md:w-1/2' alt="" />
+        <img src="src/assets/menu_page-0002.jpg" className='w-full h-full md:w-1/2' alt="" />
+        <img src="src/assets/menu_page-0003.jpg" className='w-full h-full md:w-1/2' alt="" />
+        <img src="src/assets/menu_page-0004.jpg" className='w-full h-full md:w-1/2' alt="" />
+        <img src="src/assets/menu_page-0005.jpg" className='w-full h-full md:w-1/2' alt="" />
+        <img src="src/assets/menu_page-0006.jpg" className='w-full h-full md:w-1/2' alt="" />
+        <img src="src/assets/menu_page-0007.jpg" className='w-full h-full md:w-1/2' alt="" />
+    </div>
+  )
+}
+
+export default Page
