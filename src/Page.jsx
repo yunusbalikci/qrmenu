426781d267fcc49899981c70d7afd01fda2ce0d6
yunusbalikci <balikci.yunus06@gmail.com>
2024-03-27 21:18:45 +0300

    test2

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import menu1 from './assets/menu_page-0001.jpg'
+import menu2 from './assets/menu_page-0002.jpg'
+import menu3 from './assets/menu_page-0003.jpg'
+import menu4 from './assets/menu_page-0004.jpg'
+import menu5 from './assets/menu_page-0005.jpg'
+import menu6 from './assets/menu_page-0006.jpg'
+import menu7 from './assets/menu_page-0007.jpg'
 
 function Page() {
   return (
     <div className='md:flex flex-wrap'>
         <img src={menu1} className='w-full h-full md:w-1/2' alt="" />
-        <img src="src/assets/menu_page-0002.jpg" className='w-full h-full md:w-1/2' alt="" />
-        <img src="src/assets/menu_page-0003.jpg" className='w-full h-full md:w-1/2' alt="" />
-        <img src="src/assets/menu_page-0004.jpg" className='w-full h-full md:w-1/2' alt="" />
-        <img src="src/assets/menu_page-0005.jpg" className='w-full h-full md:w-1/2' alt="" />
-        <img src="src/assets/menu_page-0006.jpg" className='w-full h-full md:w-1/2' alt="" />
-        <img src="src/assets/menu_page-0007.jpg" className='w-full h-full md:w-1/2' alt="" />
+        <img src={menu2} className='w-full h-full md:w-1/2' alt="" />
+        <img src={menu3} className='w-full h-full md:w-1/2' alt="" />
+        <img src={menu4} className='w-full h-full md:w-1/2' alt="" />
+        <img src={menu5} className='w-full h-full md:w-1/2' alt="" />
+        <img src={menu6} className='w-full h-full md:w-1/2' alt="" />
+        <img src={menu7} className='w-full h-full md:w-1/2' alt="" />
     </div>
   )
 }
